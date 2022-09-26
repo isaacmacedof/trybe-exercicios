9d5e3b07732bb23e0debbe5222b9498516c53659
Isaac Macedo <isaacmacedotr@gmail.com>
2022-09-26 16:35:41 -0300

    feat: :sparkles: exercices

--- fundamentos/Bloco-4/dia-2/scripts.js
+++ fundamentos/Bloco-4/dia-2/scripts.js
@@ -9,3 +9,13 @@ for (let i = 0; i < itens.length; i += 1) {
   console.log(cardapio);
   }
 }
+
+let carteira = 20
+let carrinho = []
+
+for (let i = 0; i < itens.length; i+= 1) {
+  if (preços[i] < carteira && i < itens.length -1) {
+    carrinho += itens[i] + ", "
+  } else if (preços [i] < carteira) 
+   {carrinho += itens[i] + ". "}
+} console.log(carrinho);
